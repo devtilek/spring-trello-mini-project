c52305d2083d483474ad5787310978e10953740a
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-07 12:54:17 +0500

    feat: validate category input

--- src/main/java/practice/trello/DTO/TaskCategoryDTO.java
+++ src/main/java/practice/trello/DTO/TaskCategoryDTO.java
@@ -1,5 +1,7 @@
 package practice.trello.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,5 +9,8 @@ import lombok.Setter;
 @Setter
 public class TaskCategoryDTO {
     private Long id;
+
+    @NotBlank(message = "Category name is required")
+    @Size(max = 100, message = "Category name must not exceed 100 characters")
     private String name;
 }
