af8e6b00ed9214053435ed24ca152b6dacfe615e
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-07 12:54:07 +0500

    feat: validate folder input

--- src/main/java/practice/trello/DTO/FolderDTO.java
+++ src/main/java/practice/trello/DTO/FolderDTO.java
@@ -1,14 +1,21 @@
 package practice.trello.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import practice.trello.Entity.TaskCategories;
 
 import java.util.List;
+
 @Getter
 @Setter
 public class FolderDTO {
     private Long id;
+
+    @NotBlank(message = "Folder name is required")
+    @Size(max = 100, message = "Folder name must not exceed 100 characters")
     private String name;
+
     private List<TaskCategories> taskCategoriesList;
 }
